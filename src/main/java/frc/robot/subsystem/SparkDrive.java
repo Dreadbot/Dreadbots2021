567f144927bf0227f8d327e22752240a8df2ea5d
cjohnson <collin.g.johnson@gmail.com>
2021-01-22 21:46:44 -0500

    Final working version for merge to main

--- src/main/java/frc/robot/subsystem/SparkDrive.java
+++ src/main/java/frc/robot/subsystem/SparkDrive.java
@@ -37,31 +37,48 @@ public class SparkDrive {
             motor.set(0.0d);
     }
 
+    /**
+     * An improved and more readable version of the Dreadbot's homemade tank
+     * drive function.
+     * 
+     * @param forwardAxisFactor    The forward factor of the drivetrain control.
+     * @param rotationAxisFactor   The rotational factor of the drivetrain control.
+     * @param finalValueMultiplier The final multiplier of the result of the
+     *                             function.
+     * @param joystickDeadband     The applied joystick deadband.
+     */
     public void tankDrive(double forwardAxisFactor, 
-                          double rotationAxis, 
-                          final double finalValueMultiplier, 
+                          double rotationAxisFactor, 
+                          final double finalValueMultiplier,
                           final double joystickDeadband) {
         double[] speedControllerOutputs = new double[4];
 
+        // Clamp Values to Acceptable Ranges (between -1.0 and 1.0).
         forwardAxisFactor = DreadbotMath.clampValue(forwardAxisFactor, -1.0d, 1.0d);
-        rotationAxis = DreadbotMath.clampValue(rotationAxis, -1.0d, 1.0d);
+        rotationAxisFactor = DreadbotMath.clampValue(rotationAxisFactor, -1.0d, 1.0d);
         
+        // Apply an Optional Joystick Deadband
         forwardAxisFactor = DreadbotMath.applyDeadbandToValue(forwardAxisFactor, -joystickDeadband, joystickDeadband, 0.0d);
-        rotationAxis = DreadbotMath.applyDeadbandToValue(rotationAxis, -joystickDeadband, joystickDeadband, 0.0d);
-
-        double leftFinalSpeed = -forwardAxisFactor + rotationAxis;
-        double rightFinalSpeed = forwardAxisFactor + rotationAxis;
+        rotationAxisFactor = DreadbotMath.applyDeadbandToValue(rotationAxisFactor, -joystickDeadband, joystickDeadband, 0.0d);
         
+        // Essential Drive Math based on the two movement factors.
+        double leftFinalSpeed = -forwardAxisFactor + rotationAxisFactor;
+        double rightFinalSpeed = forwardAxisFactor + rotationAxisFactor;
+        
+        // Assign each motor value to the output side it's on.
         speedControllerOutputs[0] = leftFinalSpeed;
         speedControllerOutputs[1] = rightFinalSpeed;
         speedControllerOutputs[2] = leftFinalSpeed;
         speedControllerOutputs[3] = rightFinalSpeed;
-
+        
+        // Add the final multiplier to the values.
         for(int i = 0; i < speedControllerOutputs.length; i++)
             speedControllerOutputs[i] *= finalValueMultiplier;
-
+        
+        // Normalize the values to become between 1.0 and -1.0.
         speedControllerOutputs = DreadbotMath.normalizeValues(speedControllerOutputs);
-
+        
+        // Assign each value of the array to the motor output.
         for(int i = 0; i < speedControllerOutputs.length; i++)
             motors.get(i).set(speedControllerOutputs[i]);
     }
