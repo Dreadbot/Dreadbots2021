9836ef838ca1381969ad5036dd0098bf38f02a58
schelcc <schelcc@pobox.com>
2021-01-28 19:11:40 -0500

    setting/calibration code for vision

--- vision/vision2021.py
+++ vision/vision2021.py
@@ -1,3 +1,43 @@
-import cv2
+import json
 
-print("heck yeah vision file is a thing")
+#All calis must be lowercase
+def readCalibration(setting=None):
+    #Checking if the call used the setting flag
+    if setting is not None:
+        #If they did, make the string lowercase as thats the convention
+        setting.lower()
+    else:
+        setting = "default"
+
+    #Open the calibrations file
+    with open("calibrations.json", "r") as f:
+        #Read the file
+        dump = f.read()
+        calibrations = json.loads(dump)
+        f.close()
+    
+    return(calibrations[setting])
+
+def addCalibration(settingName, hue, sat, lum):
+    #Open the calibrations file
+    with open("calibrations.json", "r") as f:
+        #Read the file
+        dump = f.read()
+        calibrations = json.loads(dump)
+        f.close()
+    
+    #Create the dictionary to add to the calibrations
+    calibration = {
+        settingName : {
+            "hue" : hue,
+            "sat" : sat,
+            "lum" : lum
+        }
+    }
+
+    #Add this calibration on top of the prev ones
+    calibrations.update(calibration)
+
+    #Write over the calibration file with the new calibration file
+    with open("calibrations.json", "w+") as f:
+        json.dump(calibrations, f, indent=4)
