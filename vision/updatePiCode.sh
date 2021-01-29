e50ea2ecf1ae30fa6c4a1ca1de2b6547697d3998
schelcc <schelcc@pobox.com>
2021-01-29 09:48:43 -0500

    run script for pycharm as well as code copy

--- vision/updatePiCode.sh
+++ vision/updatePiCode.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-echo "This will ask for password, get cole if you don't know it"
-echo "Run this from the main dir of the project"
-git pull
-cd vision/
-scp vision2021.py pi@1.2.3.4
-#temp ip, waiting on static
+echo "Must know pi's password!!!"
+echo "If you do not, contact cole"
+while test $# -gt 0; do
+  case "$1" in
+    -h)
+      shift
+      echo "-p : Pull the code only (don't run)"
+      echo "-r : Run the code without pulling"
+  esac
+  case "$1" in
+    -r)
+      shift
+      scp vision2021.py #dest-ip
+      ssh pi@ #dest- ip # python3 vision2021.py
+  esac
+  case "$1" in
+    -p)
+      shift
+      git pull
+      scp vision2021.py #dest-ip
+  esac
+done
